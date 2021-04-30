3e595701b801da9d887b391a8d9c5a9f8623e3be
Fabio Buehler <fabiobuehler10@gmail.com>
2021-04-30 15:08:20 +0200

    live update

--- live.py
+++ live.py
@@ -40,10 +40,11 @@ class Gui():
         self.app = dash.Dash(__name__)
 
         title = html.Div(id="title", children=[html.H1(f"Trading {self.hook.ldb.symbol}, on the {self.hook.ldb.market_endpoint} market")])
+        profit = html.Div(id="profit")
         live_graph = html.Div(id="live-graph-wrapper")
         interval = dcc.Interval(id='interval', interval=1*1000, n_intervals=0)
 
-        self.app.layout = html.Div(children=[title, live_graph, interval])
+        self.app.layout = html.Div(children=[title, profit, live_graph, interval])
 
         @self.app.callback(Output('live-graph-wrapper', 'children'),
                            Input('interval', 'n_intervals'))
@@ -59,6 +60,14 @@ class Gui():
             fig.add_trace(go.Scatter(x=data["close_time"], y=data["sell"], mode="markers", name="sell", line=dict(color="red")))
             
             return dcc.Graph(id="live-graph", figure=fig)
+
+        @self.app.callback(Output('profit', 'children'),
+                           Input('interval', 'n_intervals'))
+        def update_profit(n):
+            #get the specific profit
+            specific_profit = self.hook.broker.specific_profit
+            
+            return html.H2(f"Specific Profit since start: {specific_profit}")
 
     def run(self):
         self.app.run_server(host="0.0.0.0", debug=False, dev_tools_silence_routes_logging=True)
@@ -125,21 +134,99 @@ class ActionLog():
 
 class Broker():
 
-    def __init__(self, config_path=None):
-        
-        
+    def __init__(self, symbol, testing=True, config_path=None):
+        #save/create neccessary variables
+        self.symbol = symbol
+        self.testing = testing
+        self.profit = 0
+        self.specific_profit = 0
+        self.mode = "buy"
+
         #load in the config
-        self.config = read_config(path=config_path)
+        self.config = read_config(path=config_path)        
 
         #create the client
-        self.client = Client(api_key=self.config["binance"]["key_testnet"], api_secret=self.config["binance"]["secret_testnet"])
-        self.client.API_URL = "https://testnet.binance.vision/api"
+        self.client = Client(api_key=self.config["binance"]["key"], api_secret=self.config["binance"]["secret"])
+        
+        """
+        Testnet:
+            self.client = Client(api_key=self.config["binance"]["key_testnet"], api_secret=self.config["binance"]["secret_testnet"])
+            
+            self.client.API_URL = "https://testnet.binance.vision/api"
 
-        order = self.client.create_order(symbol="ETHUSDT", side=SIDE_SELL, type=ORDER_TYPE_MARKET, quantity=2)
-        print(order)
+            order = self.client.create_order(symbol="ETHUSDT", side=SIDE_SELL, type=ORDER_TYPE_MARKET, quantity=2)
+            print(order)
 
-        print(self.client.get_asset_balance(asset="ETH"))
-        print(self.client.get_asset_balance(asset="USDT"))
+            print(self.client.get_asset_balance(asset="ETH"))
+            print(self.client.get_asset_balance(asset="USDT"))
+        """
+    
+    def _get_current_price(self):
+        market_endpoint = self.config["binance"]["market_endpoint"]
+        
+        if market_endpoint == "spot":
+            price_dict = self.client.get_symbol_ticker(symbol=self.symbol)
+            price = price_dict["price"]
+        elif market_endpoint == "futures":
+            price_dict = self.client.futures_symbol_ticker(symbol=self.symbol)
+            price = price_dict["price"]
+        else:
+            raise Exception(f"Your chosen market endpoint: {market_endpoint} is not available, change in config.json")
+        
+        print(price)
+
+        return float(price)
+
+    def buy(self, amount):
+        if self.testing:
+            return self._test_buy(amount=amount)
+        
+        raise Exception("Real buying has not been implemented yet")
+
+        return
+
+    def _test_buy(self, amount):
+        if self.mode == "buy":
+            #get the current price
+            price = self._get_current_price()
+            
+            #set as buyprice
+            self.buy_price = price
+
+            self.mode = "sell"
+        else:
+            return
+
+    def sell(self):
+        if self.testing:
+            return self._test_sell()
+        
+        raise Exception("Real selling has not been implemented yet")
+
+    def _test_sell(self):
+        if self.mode == "sell":
+            #get the current price
+            price = self._get_current_price()
+            
+            #calculate profit
+            specific_profit = price/self.buy_price * (1-0.00075)**2 - 1
+
+            #add to specific profit count
+            self.specific_profit += specific_profit
+
+            self.mode = "buy"
+        else:
+            return
+
+    def trade(self, action, amount):
+        if action == 0:
+            return
+        elif action == 1:
+            self.buy(amount=amount)
+        elif action == 2:
+            self.sell()
+        else:
+            raise Exception(f"Your chosen action: {action} is not valid")
 
 class Bot():
 
@@ -164,11 +251,11 @@ class Bot():
         #setup the actionlog
         self.actionlog = ActionLog(size=100)
 
+        #setup the broker
+        self.broker = Broker(symbol=self.symbol, testing=True)
+
         #setup the gui
         self.gui = Gui(hook=self)
-
-        #performance values
-        self.profit = 0
 
     def update(self):
         start = time.time()
@@ -200,7 +287,7 @@ class Bot():
         action = self.actor.get_action(state)
 
         #do something with this action
-        print(action)
+        self.broker.trade(action=action, amount=1000)
 
         #save the action
         self.actionlog.append(action=action, actual_price=100)
